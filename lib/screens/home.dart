e9017afb8263a9b62c03b218c37a8237b04a75fc
skai-shiroe <skaisenou@gmail.com>
2024-07-22 02:54:22 +0000

    add and delete feature with network assets

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:otaku/screens/add-manga.dart';
 import 'package:otaku/screens/manga-info.dart';
 
 class HomePage extends StatelessWidget {
@@ -7,8 +8,23 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color.fromARGB(197, 1, 44, 61),
       appBar: AppBar(
-        title: const Text('Manga Store'),
+        title: const Text(
+          'Manga Store',
+          style: TextStyle(),
+        ),
+        centerTitle: true,
+        leading: IconButton(
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (context) => AddPage(),
+                  fullscreenDialog: true,
+                ),
+              );
+            },
+            icon: const Icon(Icons.add)),
       ),
       body: MangaInformation(),
     );
--- lib/screens/manga-info.dart
+++ lib/screens/manga-info.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 class MangaInformation extends StatefulWidget {
@@ -13,6 +14,36 @@ class _MangaInformationState extends State<MangaInformation> {
   final Stream<QuerySnapshot> animeStream =
       FirebaseFirestore.instance.collection('Anime').snapshots();
 
+  // fonction pour mettre a jour le nombre de like
+
+  void addLike(String docID, int likes) {
+    var newLikes = likes + 1;
+    try {
+      FirebaseFirestore.instance
+          .collection('Anime')
+          .doc(docID)
+          // ignore: avoid_print
+          .update({'likes': newLikes}).then((value) => print('donnee a jour'));
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+
+  // fonction pour supprimer un document specifique
+
+  void deleteManga(String docID) {
+    try {
+      FirebaseFirestore.instance
+          .collection('Anime')
+          .doc(docID)
+          .delete()
+          // ignore: avoid_print
+          .then((value) => print('Document supprimé'));
+    } catch (e) {
+      print('Erreur lors de la suppression du document : $e');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return StreamBuilder<QuerySnapshot>(
@@ -20,21 +51,86 @@ class _MangaInformationState extends State<MangaInformation> {
       builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
         // Affichage d'un message de chargement pendant que les données sont récupérées.
         if (snapshot.hasError) {
-          return Text('Something went wrong');
+          return const Text('Something went wrong');
         }
 
         if (snapshot.connectionState == ConnectionState.waiting) {
-          return Text("Loading");
+          return const Text("Loading");
         }
 
         return ListView(
           children: snapshot.data!.docs.map((DocumentSnapshot document) {
-            Map<String, dynamic> data =
+            Map<String, dynamic> manga =
                 document.data()! as Map<String, dynamic>;
-            return ListTile(
-              trailing: Image.network(data['poster']),
-              title: Text(data['name']),
-              subtitle: Text(data['poster']),
+            return Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                children: [
+                  SizedBox(
+                    width: 100,
+                    child: Image.network(
+                      manga['poster'],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          manga['name'],
+                          style: const TextStyle(
+                              fontSize: 18, fontWeight: FontWeight.bold),
+                        ),
+                        const Text('Annee de production'),
+                        Text(
+                          manga['year'].toString(),
+                          style: const TextStyle(fontSize: 16),
+                        ),
+                        Row(
+                          children: [
+                            for (final categorie in manga['categories'])
+                              Padding(
+                                padding: const EdgeInsets.only(right: 5),
+                                child: Chip(
+                                  backgroundColor:
+                                      Color.fromARGB(103, 37, 113, 175),
+                                  label: Text(categorie),
+                                ),
+                              )
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            IconButton(
+                              padding: EdgeInsets.zero,
+                              constraints: const BoxConstraints(),
+                              icon: const Icon(Icons.favorite),
+                              iconSize: 20,
+                              onPressed: () {
+                                addLike(document.id, manga['likes']);
+                              },
+                            ),
+                            Text(manga['likes'].toString()),
+                            IconButton(
+                              padding: const EdgeInsets.only(left: 50),
+                              constraints: const BoxConstraints(),
+                              icon: const Icon(Icons.delete_outline),
+                              iconSize: 20,
+                              onPressed: () {
+                                deleteManga(document.id);
+                              },
+                            ),
+                            const SizedBox(
+                              width: 5,
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
             );
           }).toList(),
         );
